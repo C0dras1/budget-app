978ccf77d55711091c23a3586ccd2955ece6c008
Jesse <jessesuderman@gmail.com>
2020-07-03 12:11:26 -0700

    functionality to add items and clear fields w/ focus

--- starter/app.js
+++ starter/app.js
@@ -21,21 +21,37 @@ const budgetController = (function() {
       exp: 0,
       inc: 0
     }
-  }
+  };
 
   return {
     addItem: function(type, des, val) {
       let newItem, ID;
 
-      ID = 0;
+      //create ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1; //ID = last ID + 1;
+      } else {
+        ID = 0;
+      }
+      
 
+      //create new item based on 'inc' or 'exp' type
       if (type === 'exp') {
         newItem = new Expense(ID, des, val);
       } else if (type === 'inc') {
         newItem = new Income(ID, des, val);
       }
+      //push into data structure
+      data.allItems[type].push(newItem);
+      //return new ID
+      return newItem;
       
+    },
+    testing: function() {
+      console.log(data);
     }
+
+
   }
 })();
 
@@ -44,7 +60,9 @@ const UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
   //public method to get accessed by other modules
   return {
@@ -54,6 +72,35 @@ const UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    addListItem: function(obj, type) {
+      let html, newHtml, element;
+      // 1. create html string with placeholder text
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+      // 2. replace placeholder text with data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+      // 3. insert html into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+    clearFields: function() {
+      let fields, fieldsArray;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArray = Array.prototype.slice.call(fields);
+      fieldsArray.forEach(el => {
+        el.value = "";
+      });
+
+      fieldsArray[0].focus();
     },
     getDOMstrings: function() {
       return DOMstrings;
@@ -76,12 +123,18 @@ const appController = (function(budgetCtrl, UICtrl) {
   }
 
   const crtlAddItem = function() {
+    let input, newItem;
+
     //get input data
-    let input = UIController.getInput();
+    input = UIController.getInput();
 
     //add item to the budget controller
-
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //add the new item to the interface
+    UICtrl.addListItem(newItem, input.type);
+
+    //clear the fields
+    UICtrl.clearFields();
 
     //calculate the budget
 
